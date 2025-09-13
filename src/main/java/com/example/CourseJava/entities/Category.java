c880738991d0f3c59a627378957119ef08a0ec9d
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-09-13 23:46:36 +0100

    Many-to-many association with JoinTable

--- src/main/java/com/example/CourseJava/entities/Category.java
+++ src/main/java/com/example/CourseJava/entities/Category.java
@@ -5,10 +5,13 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,20 +19,20 @@ import jakarta.persistence.Table;
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
+
 	public Category() {
-		
 	}
-	
+
 	public Category(Long id, String name) {
-		super();
 		this.id = id;
 		this.name = name;
 	}
@@ -49,7 +52,7 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Set<Product> getProducts() {
 		return products;
 	}
@@ -70,9 +73,5 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-	
-	
-
 }
+
--- src/main/java/com/example/CourseJava/entities/Product.java
+++ src/main/java/com/example/CourseJava/entities/Product.java
@@ -9,13 +9,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,19 +28,22 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 	
-	private Set<Category> categories = new HashSet<>();
 	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+		joinColumns = @JoinColumn(name = "product_id"),
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+
 	public Product() {
-		
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.imgUrl = imgUrl;	
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -99,8 +106,4 @@ public class Product implements Serializable {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
 }
