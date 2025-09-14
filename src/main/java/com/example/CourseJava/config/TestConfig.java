a41a41d3efd86d38a5bf00634db963e06602d947
MarcoAntonioLobo <marcoantoniolobo82@gmail.com>
2025-09-14 18:31:55 +0100

    Payment, one-to-one association

--- src/main/java/com/example/CourseJava/config/TestConfig.java
+++ src/main/java/com/example/CourseJava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.CourseJava.entities.Category;
 import com.example.CourseJava.entities.Order;
 import com.example.CourseJava.entities.OrderItem;
+import com.example.CourseJava.entities.Payment;
 import com.example.CourseJava.entities.Product;
 import com.example.CourseJava.entities.User;
 import com.example.CourseJava.entities.enums.OrderStatus;
@@ -38,7 +39,7 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private OrderItemRepository orderItemRepository;
-
+    
     @Override
     public void run(String... args) throws Exception {
 
@@ -76,5 +77,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+        
+        Payment pay1 = new Payment (null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        
+        orderRepository.save(o1);
     }
 }
--- src/main/java/com/example/CourseJava/entities/Order.java
+++ src/main/java/com/example/CourseJava/entities/Order.java
@@ -10,6 +10,7 @@ import com.example.CourseJava.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,63 +18,101 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
-    private Instant moment;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant moment;
 
-    private Integer orderStatus;
+	private Integer orderStatus;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
 
-    @OneToMany(mappedBy = "id.order")
-    @JsonManagedReference
-    private Set<OrderItem> items = new HashSet<>();
+	@OneToMany(mappedBy = "id.order")
+	@JsonManagedReference
+	private Set<OrderItem> items = new HashSet<>();
 
-    public Order() {}
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment payment;
 
-    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
-        this.id = id;
-        this.moment = moment;
-        setOrderStatus(orderStatus);
-        this.client = client;
-    }
+	public Order() {
+	}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}
 
-    public Instant getMoment() { return moment; }
-    public void setMoment(Instant moment) { this.moment = moment; }
+	public Long getId() {
+		return id;
+	}
 
-    public OrderStatus getOrderStatus() { return OrderStatus.valueOf(orderStatus); }
-    public void setOrderStatus(OrderStatus orderStatus) {
-        if (orderStatus != null) this.orderStatus = orderStatus.getCode();
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public User getClient() { return client; }
-    public void setClient(User client) { this.client = client; }
+	public Instant getMoment() {
+		return moment;
+	}
 
-    public Set<OrderItem> getItems() { return items; }
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
 
-    @Override
-    public int hashCode() { return Objects.hash(id); }
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Order other = (Order) obj;
-        return Objects.equals(id, other.id);
-    }
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null)
+			this.orderStatus = orderStatus.getCode();
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
 }
